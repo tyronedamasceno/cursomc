3d44fdeba504e5de2eceef7ff3b25fb2fa7c7227
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-08-30 15:30:08 -0300

    Classes de Dominio Pedido, Pagamento e suas especializações. Mapeamento relacional um pra um

---
+++ src/main/java/com/tyrone/cursomc/domain/enums/EstadoPagamento.java
@@ -0,0 +1,39 @@
+package com.tyrone.cursomc.domain.enums;
+
+public enum EstadoPagamento {
+
+	PENDENTE(1, "Pendente"),
+	QUITADO(2, "Quitado"),
+	CANCELADO(3, "Cancelado");
+	
+	
+	private Integer cod;
+	private String descricao;
+	
+	private EstadoPagamento(Integer cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static EstadoPagamento toEnum(Integer cod) {
+		if (cod == null) {
+			return null;
+		}
+		
+		for (EstadoPagamento tipo : EstadoPagamento.values()) {
+			if (cod.equals(tipo.getCod())) {
+				return tipo;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id invalido: " + cod);
+	}
+}
