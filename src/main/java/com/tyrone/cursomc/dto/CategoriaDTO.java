c4da427c16565e170dddb0dfa6f02d36d9976c9e
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-09-03 09:36:00 -0300

    Listando todas categoria usando DTO

---
+++ src/main/java/com/tyrone/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,40 @@
+package com.tyrone.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.tyrone.cursomc.domain.Categoria;
+
+public class CategoriaDTO  implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public CategoriaDTO() {
+		
+	}
+	
+	public CategoriaDTO(Categoria obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
