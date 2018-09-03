9aa9649dadda9d9a6d57144a8a6548450ec128a1
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-09-03 11:26:07 -0300

    PUT, GET E DELETE para Cliente

---
+++ src/main/java/com/tyrone/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,61 @@
+package com.tyrone.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.tyrone.cursomc.domain.Cliente;
+
+public class ClienteDTO   implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=3, max=150, message="O tamanho deve ser entre 3 e 150 caracteres")
+	private String nome;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Email(message="Email inválido")
+	private String email;
+	
+	public ClienteDTO() {
+		
+	}
+
+	public ClienteDTO(Cliente obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.email = obj.getEmail();
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
--- src/main/java/com/tyrone/cursomc/services/ClienteService.java
+++ src/main/java/com/tyrone/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.tyrone.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.tyrone.cursomc.domain.Cliente;
+import com.tyrone.cursomc.dto.ClienteDTO;
 import com.tyrone.cursomc.repositories.ClienteRepository;
+import com.tyrone.cursomc.services.exceptions.DataIntegrityException;
 import com.tyrone.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,5 +28,37 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir pois existem entidades relacionadas");
+		}
+	}
+	
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest request = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(request);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 	
 }
