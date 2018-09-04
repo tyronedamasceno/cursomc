5bfcfe5e4d168a146532ec4f113951a3d88dd4f5
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-09-04 09:35:13 -0300

    Validação customizada - Email unico na inserção de cliente

--- src/main/java/com/tyrone/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/tyrone/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tyrone.cursomc.domain.Cliente;
 import com.tyrone.cursomc.domain.enums.TipoCliente;
 import com.tyrone.cursomc.dto.ClienteNewDTO;
+import com.tyrone.cursomc.repositories.ClienteRepository;
 import com.tyrone.cursomc.resources.exceptions.FieldMessage;
 import com.tyrone.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -31,6 +39,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod())
 				&&  !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email ja existente"));
 		}
 		
 		for (FieldMessage e : list) {
