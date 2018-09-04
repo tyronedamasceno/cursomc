844320f99973c6dc9257fddb9955e325e8ebc267
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-09-04 08:13:39 -0300

    Validação customizada do cpfOuCnpj na inserção do cliente

--- src/main/java/com/tyrone/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/tyrone/cursomc/dto/ClienteNewDTO.java
@@ -2,20 +2,41 @@ package com.tyrone.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.tyrone.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=3, max=150, message="O tamanho deve ser entre 3 e 150 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Email(message="Email inválido")
 	private String email;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String numero;
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String cep;
 	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
@@ -25,7 +46,7 @@ public class ClienteNewDTO implements Serializable{
 	public ClienteNewDTO() {
 		
 	}
-
+	
 	public String getNome() {
 		return nome;
 	}
---
+++ src/main/java/com/tyrone/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,43 @@
+package com.tyrone.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.tyrone.cursomc.domain.enums.TipoCliente;
+import com.tyrone.cursomc.dto.ClienteNewDTO;
+import com.tyrone.cursomc.resources.exceptions.FieldMessage;
+import com.tyrone.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+
+		// inclua os testes aqui, inserindo erros na lista
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod())
+				&&  !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod())
+				&&  !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
