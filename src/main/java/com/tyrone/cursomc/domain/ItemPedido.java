83193d2b0b269aca528811786bcabc673b388a36
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-09-07 22:32:51 -0300

    Implementando toString() de Pedido

--- src/main/java/com/tyrone/cursomc/domain/ItemPedido.java
+++ src/main/java/com/tyrone/cursomc/domain/ItemPedido.java
@@ -1,6 +1,8 @@
 package com.tyrone.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -109,8 +111,20 @@ public class ItemPedido  implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt","BR"));
+		StringBuilder builder = new StringBuilder();
+		builder.append(getProduto().getNome());
+		builder.append(", Qtd: ");
+		builder.append(getQuantidade());
+		builder.append(", Preço unitário: ");
+		builder.append(nf.format(getPreco()));
+		builder.append(", Subtotal: ");
+		builder.append(nf.format(getSubtotal()));
+		builder.append("\n");
+		return builder.toString();
+	}
 	
 }
