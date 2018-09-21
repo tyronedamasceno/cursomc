cfd5424ede62180d6727be8d4672fed860feb372
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-09-21 17:27:16 -0300

    Expondo header location nas respostas

--- src/main/java/com/tyrone/cursomc/services/DBService.java
+++ src/main/java/com/tyrone/cursomc/services/DBService.java
@@ -211,11 +211,11 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "nelio.cursos@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA, encoder.encode("123"));
+		Cliente cli1 = new Cliente(null, "Maria Silva", "joao@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA, encoder.encode("batata"));
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
-		Cliente cli2 = new Cliente(null, "Ana Costa", "nelio.iftm@gmail.com", "31628382740", TipoCliente.PESSOA_FISICA, encoder.encode("123"));
+		Cliente cli2 = new Cliente(null, "Ana Costa", "tyronedamasceno@gmail.com", "31628382740", TipoCliente.PESSOA_FISICA, encoder.encode("batata"));
 		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
 		cli2.addPerfil(Perfil.ADMIN);
 		
